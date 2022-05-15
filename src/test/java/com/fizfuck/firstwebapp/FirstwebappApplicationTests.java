ae3f4e8a14e24b2bf49e0a3ca982dedb3fc11d22
HrynyshynOleksii <hrunuwun@gmail.com>
2022-05-16 00:15:18 +0300

    Initial commit

---
+++ src/test/java/com/fizfuck/firstwebapp/FirstwebappApplicationTests.java
@@ -0,0 +1,12 @@
+package com.fizfuck.firstwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class FirstwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
